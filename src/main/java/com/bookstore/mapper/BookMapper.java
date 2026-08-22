17960ffc713423dad2cafee24e3a7880e63079bb
wakaka <2321352131@qq.com>
2026-08-22 19:15:16 +0800

    bookstore[charpter01]修改mapper

--- src/main/java/com/bookstore/mapper/BookMapper.java
+++ src/main/java/com/bookstore/mapper/BookMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bookstore.entity.Book;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BookMapper extends BaseMapper<Book> {
+//    List<Book> queryBookList()
 }
