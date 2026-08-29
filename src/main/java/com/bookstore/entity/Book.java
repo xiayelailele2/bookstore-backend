4a5401a0df9114f6f039bd6b8dd515ad2a26466e
wakaka <2321352131@qq.com>
2026-08-29 21:34:49 +0800

    bookstore[chapter01]后端更新

--- src/main/java/com/bookstore/entity/Book.java
+++ src/main/java/com/bookstore/entity/Book.java
@@ -10,13 +10,13 @@ import java.math.BigDecimal;
 @TableName("book")
 public class Book {
     @TableId(type = IdType.AUTO)
-    private Integer id;
-    private String bookName;
-    private String author;
-    private BigDecimal price;
-    private String category;
-    private String bookImg;
-    private String bookDesc;
-    private String press;
-    private String publishDate;
+    private Integer id;//主键ID
+    private String bookName;//图书名称
+    private String author;//图书作者
+    private BigDecimal price;//图书价格
+    private String category;//图书分类
+    private String bookImg;//图书封面
+    private String bookDesc;//图书简介
+    private String press;//出版社
+    private String publishDate;//出版日期
 }
